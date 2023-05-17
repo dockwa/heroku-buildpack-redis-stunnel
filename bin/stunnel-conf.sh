d81f563e0ad2d8df08e62d8ed763bcaa89f77e3d
micole <micole.3@gmail.com>
2023-05-17 13:49:53 -0400

    Strip away all funky stuff

--- bin/stunnel-conf.sh
+++ bin/stunnel-conf.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-URLS=${HEROKU_REDIS_BLUE_URL}
-n=1
 
 mkdir -p /app/vendor/stunnel/var/run/stunnel/
 
@@ -16,41 +14,19 @@ ciphers = HIGH:!ADH:!AECDH:!LOW:!EXP:!MD5:!3DES:!SRP:!PSK:@STRENGTH
 debug = ${STUNNEL_LOGLEVEL:-notice}
 EOFEOF
 
-for URL in $URLS
-do
-  eval URL_VALUE=\$$URL
-  PARTS=$(echo $URL_VALUE | perl -lne 'print "$1 $2 $3 $4 $5 $6 $7" if /^([^:]+):\/\/([^:]+):([^@]+)@(.*?):(.*?)(\/(.*?)(\\?.*))?$/')
-  if [ -z "$PARTS" ]
-  then
-    PARTS=$(echo $URL_VALUE | perl -lne 'print "$1 $2 $3 $4 $5 $6 $7" if /^([^:]+):\/\/:([^@]+)@(.*?):(.*?)(\/(.*?)(\\?.*))?$/')
-    WITHOUT_USERNAME=true
-  fi
-  URI=( $PARTS )
+REDIS_URL=${HEROKU_REDIS_BLUE_URL/rediss/redis}
+REDIS_URL=${REDIS_URL%@*}
+REDIS_URL=${REDIS_URL}@127.0.0.1:6371
 
-  URI_SCHEME=${URI[0]}
-  URI_USER=${URI[1]}
-  URI_PASS=${URI[2]}
-  URI_HOST=${URI[3]}
-  URI_PORT=${URI[4]}
+export REDIS_URL=$REDIS_URL
 
-  echo "URI PARTS"
-  echo ${URI}
-
-  echo "Setting REDIS_URL config var"
-  export REDIS_URL=redis://$URI_USER:$URI_PASS@127.0.0.1:637${n}
-  # echo "Setting ${URL}_STUNNEL config var"
-  # export ${URL}_STUNNEL=$URI_SCHEME://$URI_USER:$URI_PASS@127.0.0.1:637${n}
-
-  cat >> /app/vendor/stunnel/stunnel.conf << EOFEOF
+cat >> /app/vendor/stunnel/stunnel.conf << EOFEOF
 [REDIS_URL]
 client = yes
-accept = 127.0.0.1:637${n}
-connect = $URI_HOST:$URI_PORT
+accept = 127.0.0.1:6371
+connect = ${HEROKU_REDIS_BLUE_URL}
 retry = ${STUNNEL_CONNECTION_RETRY:-"no"}
 EOFEOF
-
-  let "n += 1"
-done
 
 cat /app/vendor/stunnel/stunnel.conf
 env
